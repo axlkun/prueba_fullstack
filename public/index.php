7efbda81ea8d7e9a0eb6d4a2ea5c4d7962574414
axlkun <axelcruz.dev@gmail.com>
2024-03-20 03:28:34 -0600

    add web routes

--- public/index.php
+++ public/index.php
@@ -4,13 +4,16 @@ require_once __DIR__ . '/../includes/app.php';
 require_once __DIR__ . '/../Router.php';
 require_once __DIR__ . '/../controllers/UsuarioController.php';
 require_once __DIR__ . '/../controllers/CommentController.php';
+require_once __DIR__ . '/../controllers/PaginasController.php';
 
 use MVC\Router;
 use Controllers\UsuarioController;
 use Controllers\CommentController;
+use Controllers\PaginasController;
 
 $router = new Router();
 
+// CRUD
 $router->post('/api/usuario',[UsuarioController::class, 'store']);
 $router->put('/api/usuario',[UsuarioController::class, 'update']);
 $router->delete('/api/usuario',[UsuarioController::class, 'destroy']);
@@ -20,5 +23,10 @@ $router->post('/api/comment',[CommentController::class, 'store']);
 $router->put('/api/comment',[CommentController::class, 'update']);
 $router->delete('/api/comment',[CommentController::class, 'destroy']);
 $router->get('/api/comment',[CommentController::class, 'show']);
+
+// views
+$router->get('/',[PaginasController::class,'login']);
+$router->get('/register',[PaginasController::class,'register']);
+
 
 $router->validateRoutes();
