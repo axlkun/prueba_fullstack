ad7a0c501eab9ad640efa77b10e464332a58b8e0
axlkun <axelcruz.dev@gmail.com>
2024-03-21 12:33:42 -0600

    stylig header

--- views/header.php
+++ views/header.php
@@ -1,7 +1,10 @@
-<div>
-    <p>Hola <?php echo $_SESSION['fullname'] ?></p>
-    
-    <a href="/home">Inicio</a>
-    <a href="/profile">Ver perfil</a>
-    <a href="/auth/logout">Cerrar sesion</a>
+<div class="header">
+    <div class="user-info">
+        <p>Hola <?php echo $_SESSION['fullname'] ?></p>
+    </div>
+    <div class="nav-links">
+        <a href="/home">Inicio</a>
+        <a href="/profile">Ver perfil</a>
+        <a href="/auth/logout">Cerrar sesión</a>
+    </div>
 </div>
