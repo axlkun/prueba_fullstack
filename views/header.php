2b22b2fda129f0a37ecefab32aad9be56cde0693
axlkun <axelcruz.dev@gmail.com>
2024-03-21 01:02:55 -0600

    create layout

---
+++ views/header.php
@@ -0,0 +1,7 @@
+<div>
+    <p>Hola <?php echo $_SESSION['fullname'] ?></p>
+    
+    <a href="/home">Inicio</a>
+    <a href="/profile">Ver perfil</a>
+    <a href="/auth/logout">Cerrar sesion</a>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -1,7 +1,47 @@
-<div>
-    <p>Hola <?php echo $_SESSION['fullname'] ?></p>
-    
-    <a href="/home">Inicio</a>
-    <a href="/profile">Ver perfil</a>
-    <a href="/auth/logout">Cerrar sesion</a>
-</div>
+<?php
+// Verificar si la sesión ya está activa
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$auth = $_SESSION['login'] ?? false;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Full Stack</title>
+    <!-- <link rel="stylesheet" href="../build/css/app.css"> -->
+</head>
+
+<body>
+
+    <?php
+
+    if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+
+        include __DIR__ . "/header.php";
+    }
+
+    ?>
+
+    <?php echo $contenido ?>
+
+    <footer class="footer seccion">
+        <!-- <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="/home">Inicio</a>
+            </nav>
+        </div> -->
+
+        <p class="copyright">Axel Cruz <?php echo date('Y'); ?> &copy;</p>
+    </footer>
+
+</body>
+
+</html>
